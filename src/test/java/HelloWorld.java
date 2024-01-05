2c98e54c200784d5dbddb85c33614c2e5cd5b7c3
codewiser116 <benazir.baialieva@gmail.com>
2024-01-04 21:16:07 -0600

    updated world to united states

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello United States!");
         System.out.println("This is my second change");
     }
     // this is the code which was added by another developer
